255179da4b3a2579e1ef5f691d8565ff02feb023
Badger <makonka777@gmail.com>
2024-02-23 17:45:38 +1000

    22

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
             template_name='registration/registration_form.html',
             form_class=UserCreationForm,
             success_url=reverse_lazy('login'),
-        ),name='registration',
+        ), name='registration',
     ),
 ]
 
