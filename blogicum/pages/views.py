20bc01cd332122a8428ba4e2838192ea3d4b852a
Badger <makonka777@gmail.com>
2024-02-23 18:51:24 +1000

    22

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -19,5 +19,5 @@ def page_500(request):
     return render(request, 'pages/500.html', status=500)
 
 
-def csrf_failure(request):
+def csrf_failure(request, reason=""):
     return render(request, 'pages/403csrf.html', status=403)
