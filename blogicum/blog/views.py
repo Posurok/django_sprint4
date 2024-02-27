c5f949839e25d04844f8b1d5e0a1c1c1bf69960f
Badger <makonka777@gmail.com>
2024-02-27 12:13:08 +1000

    22

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -67,10 +67,10 @@ class PostDetailView(DetailView):
 
         obj = super().get_object(queryset)
 
-        if (obj.author != self.request.user and
-                (not obj.is_published or
-                 not obj.category.is_published or
-                 obj.pub_date > timezone.now())):
+        if ((obj.author != self.request.user)
+                and (not obj.is_published
+                     or not obj.category.is_published
+                     or obj.pub_date > timezone.now())):
             raise Http404
 
         return obj
